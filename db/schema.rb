e33b596f60748db64909992f35a864cd98d310c5
Maikowazowski <maikot3587@gmail.com>
2021-07-10 15:35:32 +0900

    マイグレーション修正2

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_113917) do
+ActiveRecord::Schema.define(version: 2021_07_04_104126) do
 
+<<<<<<< Updated upstream
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "room_id", null: false
@@ -23,6 +24,10 @@ ActiveRecord::Schema.define(version: 2021_07_04_113917) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
+=======
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+>>>>>>> Stashed changes
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -40,6 +45,4 @@ ActiveRecord::Schema.define(version: 2021_07_04_113917) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "room_users", "rooms"
-  add_foreign_key "room_users", "users"
 end
